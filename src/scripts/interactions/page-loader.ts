98db519408729b95a6615a2a3ca9f7bca4a492ad
Adan-Alvarado <alvaradoadan55@gmail.com>
2026-09-03 22:04:28 -0600

    chore: set loader duration to two seconds

--- src/scripts/interactions/page-loader.ts
+++ src/scripts/interactions/page-loader.ts
@@ -1,6 +1,6 @@
 const LOCALE_TRANSITION_KEY = 'portfolio:locale-transition';
 const MAX_FONT_WAIT = 1200;
-const PROGRESS_DURATION = 2600;
+const PROGRESS_DURATION = 1700;
 const COMPLETE_DELAY = 300;
 const LOCALE_FADE_DURATION = 380;
 
